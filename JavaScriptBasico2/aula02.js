1943f4dfb87326b510b533101baebb949e45d0bd
unknown <joaomello0016@gmail.com>
2023-02-28 17:55:09 -0300

    aula de ForEach

--- JavaScriptBasico2/aula02.js
+++ JavaScriptBasico2/aula02.js
@@ -1,5 +1,31 @@
-//foreach
-
 var cursos = [
-    'Programação para leigos'
+    'Programação para Leigos',
+    'Algoritmos e Lógica de Programação',
+    'Programação em C',
+    'Programação em Java',
+    'Programação em Python',
+    'Banco de Dados',
+    'Programação Web com Django Framework',
+    'Programação em JavaScript'
 ]
+
+console.log(cursos)
+console.log(cursos.length)
+
+//definir uma função
+function imprimir(curso, indice){
+    console.log(indice + '-' + curso)
+}
+
+// para cada
+cursos.forEach(imprimir)
+
+// utilização de uma funçãom anônima (lambda/callback) e template string
+
+cursos.forEach(function(cursos, indice){
+    //console.log(`${indice} - ${cursos}`)
+})
+
+for(var i = 0; i < cursos.length; i++){
+    console.log(i + ' - ' + cursos[i])
+}
